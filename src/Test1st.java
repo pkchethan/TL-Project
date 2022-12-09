8e5c15531b47c61aae8ae1c4d34c37b6e8aaba0c
PKChethan <krupapk1988@gmail.com>
2022-12-09 13:03:46 +0530

    First Commit by TL

---
+++ src/Test1st.java
@@ -0,0 +1,12 @@
+
+public class Test1st {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Basic project created by lead");
+		
+	}
+
+}
